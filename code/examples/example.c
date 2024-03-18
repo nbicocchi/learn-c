115c2d369cbb3d40aeef0f0e0ad8f69632e0e7a6
Nicola Bicocchi <nbicocchi@unimore.it>
2024-03-18 19:32:03 +0100

    update

--- code/examples/example.c
+++ code/examples/example.c
@@ -6,5 +6,5 @@
 int main(void) {
     char str[1024];
     sprintf(str, "%f;%f;%f", 3.3, 5.66, 3.44);
-    fwrite((void *)str, strlen(str), 1, stdout)
+    fwrite((void *)str, strlen(str), 1, stdout);
 }
